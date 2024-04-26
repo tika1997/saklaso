f88c3793b669f18286d837b7b8572aa62906d2e7
tika1997 <nazgaidzetika@gmail.com>
2024-04-26 17:23:31 +0400

    davaleba

---
+++ scripts/app.js
@@ -0,0 +1,32 @@
+console.info("ჩემს შესახებ");
+let FirstName = "Tika";
+let LastName = "Nazghaidze";
+let MyAge = "26";
+let MyPhone = "599997551";
+let Email ="nazgaidzetika@gmail.com";
+let HavePet = true;
+console.log(FirstName);
+console.log(LastName);
+console.log(MyAge);
+console.log(MyPhone);
+console.log(Email);
+console.log(HavePet);
+console.log( `Hello ,i am  ${FirstName} ${LastName}`)
+
+let FavActress ="Julia Roberts";
+let HerAge ="57";
+let IsFrom ="USA";
+console.log(FavActress);
+console.log(HerAge);
+console.log(IsFrom);
+console.warn("სახელის შეცვლა");
+FavActress = "Angelina Jolie";
+console.log(FavActress);
+const Film ="Mr and Mrs Smiths";
+console.log(Film);
+let a = 26;
+let b = 57;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
